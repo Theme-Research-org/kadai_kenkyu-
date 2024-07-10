4fdc7a6fc32a35e8fe6ac1b550db68ad71d726fc
it0maki_ey <104641043+ikafurai19706@users.noreply.github.com>
2024-07-10 22:57:53 +0900

    remove caches

--- Unreal_Project/Source/Theme_Research/Theme_Research.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Theme_Research.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Theme_Research, "Theme_Research" );

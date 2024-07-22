d9f298b3debfed785a98b7f71c0261fb6931e05a
it0maki_ey <ikafurai19706@gmail.com>
2024-07-22 15:57:29 +0900

    update build.cs

--- Unreal_Project/Theme_Research/Source/Theme_Research/Theme_Research.Build.cs
+++ Unreal_Project/Theme_Research/Source/Theme_Research/Theme_Research.Build.cs
@@ -8,7 +8,7 @@ public class Theme_Research : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HTTP", "Json", "JsonUtilities" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HTTP", "Json", "JsonUtilities", , "EditorFramework" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 

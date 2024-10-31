16f118c69eda9d87b3c70ea3081d138c3b14a698
it0maki_ey <ikafurai19706@gmail.com>
2024-10-31 18:44:40 +0900

    Aspect Ratio Control

---
+++ unity/theme-research/Assets/Scripts/AspectController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class AspectRatioController : MonoBehaviour
+{
+    public bool fitToWidth;  // Widthに合わせる場合はtrue、Heightに合わせる場合はfalse
+    private RectTransform _rectTransform;
+    private RectTransform _parentRectTransform;
+
+    void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _parentRectTransform = transform.parent.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        Debug.Log("Width: " + _parentRectTransform.rect.width + " Height: " + _parentRectTransform.rect.height);
+        float newScale = _parentRectTransform.rect.height / _rectTransform.rect.height;
+        _rectTransform.sizeDelta *= new Vector2(newScale, newScale);
+    }
+}
